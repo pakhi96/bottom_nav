adf044cc017cec6f823eb29561dc6658e20983cd
pakhi agarwal <pakhi1602@gmail.com>
2017-05-26 22:18:52 +0530

    create the fragment skeleton

---
+++ app/src/main/java/com/example/pakhi/bottomnav/frag3.java
@@ -0,0 +1,29 @@
+package com.example.pakhi.bottomnav;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class frag3 extends Fragment {
+
+
+    public frag3() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag3, container, false);
+    }
+
+}
